29bc330fcc9c4d2a2d7e52a249e30e501407883a
Silvia Valdez <silvia.valdez.e@gmail.com>
2018-08-31 20:15:31 -0600

    SampleApp: Dummy functionality for FormActivity.

--- Projects/SampleApp/app/src/main/java/com/hunabsys/sampleapp/views/activities/FormActivity.kt
+++ Projects/SampleApp/app/src/main/java/com/hunabsys/sampleapp/views/activities/FormActivity.kt
@@ -1,11 +1,16 @@
 package com.hunabsys.sampleapp.views.activities
 
 import android.os.Bundle
+import android.os.Handler
+import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import android.widget.ArrayAdapter
 import com.hunabsys.sampleapp.R
 import com.hunabsys.sampleapp.helpers.AnimationHelper
 import kotlinx.android.synthetic.main.activity_form.*
+
+private const val DELAY = 1000L
 
 class FormActivity : AppCompatActivity() {
 
@@ -15,6 +20,7 @@ class FormActivity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         setUpGenders()
+        setUpSaveButton()
     }
 
     override fun onSupportNavigateUp(): Boolean {
@@ -32,5 +38,56 @@ class FormActivity : AppCompatActivity() {
         val genders = resources.getStringArray(R.array.form_genders)
         val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, genders)
         form_spinner_gender.adapter = adapter
+    }
+
+    private fun hasEmptyFields(): Boolean {
+        return form_edit_name.text.isBlank() ||
+                form_edit_last_name.text.isBlank() ||
+                form_edit_email.text.isBlank() ||
+                form_edit_age.text.isBlank() ||
+                form_edit_address.text.isBlank() ||
+                form_edit_city.text.isBlank() ||
+                form_edit_country.text.isBlank() ||
+                form_edit_job.text.isBlank()
+    }
+
+    private fun showProgress(show: Boolean) {
+        if (show) {
+            form_progress.visibility = View.VISIBLE
+            form_button_save.isEnabled = false
+        } else {
+            form_progress.visibility = View.GONE
+            form_button_save.isEnabled = true
+        }
+    }
+
+    private fun saveData() {
+        // TODO: Missing implementation of a real database
+        showProgress(true)
+
+        // Dummy functionality
+        Handler().postDelayed({
+            showProgress(false)
+
+            Snackbar.make(form_layout,
+                    getString(R.string.message_all_saved),
+                    Snackbar.LENGTH_SHORT).show()
+
+            Handler().postDelayed({
+                finish()
+            }, DELAY)
+        }, DELAY)
+    }
+
+    private fun setUpSaveButton() {
+        form_button_save.setOnClickListener {
+            if (hasEmptyFields()) {
+                Snackbar.make(form_layout,
+                        getString(R.string.error_all_fields_required),
+                        Snackbar.LENGTH_SHORT).show()
+            } else {
+                saveData()
+            }
+        }
     }
 }
