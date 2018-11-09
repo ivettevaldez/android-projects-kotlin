dc3fab8b95b69ae5e51224595f2d8937feb307e7
Silvia Valdez <silvia.valdez.e@gmail.com>
2018-11-09 16:57:20 -0700

    SampleApp: WIP: Implementation of TypefaceHelper.

--- Projects/SampleApp/app/src/main/java/com/silviavaldez/sampleapp/views/activities/MainActivity.kt
+++ Projects/SampleApp/app/src/main/java/com/silviavaldez/sampleapp/views/activities/MainActivity.kt
@@ -2,12 +2,17 @@ package com.silviavaldez.sampleapp.views.activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
+import android.support.v7.app.ActionBar
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.RelativeLayout
+import android.widget.TextView
 import com.silviavaldez.sampleapp.R
 import com.silviavaldez.sampleapp.helpers.AnimationHelper
 import com.silviavaldez.sampleapp.helpers.SignOutHelper
+import com.silviavaldez.sampleapp.helpers.TypefaceHelper
 import io.realm.Realm
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -20,6 +25,7 @@ class MainActivity : AppCompatActivity() {
         Realm.init(this)
 
         setListenersToMenuButtons()
+        setUpTypefaces()
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -52,5 +58,30 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
             AnimationHelper().enterTransition(this)
         }
+    }
+
+    private fun setUpActionBar() {
+        val layoutParams = RelativeLayout.LayoutParams(ActionBar.LayoutParams.WRAP_CONTENT,
+                ActionBar.LayoutParams.WRAP_CONTENT)
+
+        val textView = TextView(applicationContext)
+        textView.layoutParams = layoutParams
+        textView.text = title
+        textView.textSize = 20f
+        textView.setTextColor(ContextCompat.getColor(applicationContext, R.color.white))
+
+        val typeRegular = TypefaceHelper(this).regular
+        textView.typeface = typeRegular
+
+        supportActionBar?.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
+        supportActionBar?.customView = textView
+    }
+
+    private fun setUpTypefaces() {
+        val typefaceHelper = TypefaceHelper(this)
+        main_button_list.typeface = typefaceHelper.black
+        main_button_accelerometer.typeface = typefaceHelper.black
+
+
     }
 }
--- Projects/SampleApp/app/src/main/java/com/silviavaldez/sampleapp/views/activities/SplashActivity.kt
+++ Projects/SampleApp/app/src/main/java/com/silviavaldez/sampleapp/views/activities/SplashActivity.kt
@@ -1,23 +1,26 @@
 package com.silviavaldez.sampleapp.views.activities
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
-import android.support.v7.app.AppCompatActivity
 import com.silviavaldez.sampleapp.R
 import com.silviavaldez.sampleapp.helpers.AnimationHelper
 import com.silviavaldez.sampleapp.helpers.PreferencesHelper
+import com.silviavaldez.sampleapp.helpers.TypefaceHelper
 import com.silviavaldez.sampleapp.helpers.UtilHelper
+import kotlinx.android.synthetic.main.activity_splash.*
 
 private const val DELAY: Long = 2000L
 
-class SplashActivity : AppCompatActivity() {
+class SplashActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
         UtilHelper().changeStatusBarColor(this)
+        splash_text_app_name.typeface = TypefaceHelper(this).bold
 
         Handler().postDelayed({
             validateSession()
