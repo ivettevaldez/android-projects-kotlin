c5ef23b51a02d009dfd5e3298c3541c1beb2149f
Ivette Valdez <ivette.valdez.e@gmail.com>
2020-03-26 13:24:00 +0100

    Multithreading: Solution for exercise 3.

--- Projects/Multithreading/app/src/main/java/com/ivettevaldez/multithreading/exercises/Exercise3Fragment.java
+++ Projects/Multithreading/app/src/main/java/com/ivettevaldez/multithreading/exercises/Exercise3Fragment.java
@@ -1,6 +1,9 @@
 package com.ivettevaldez.multithreading.exercises;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,8 +21,14 @@ import com.ivettevaldez.multithreading.R;
  */
 public class Exercise3Fragment extends Fragment {
 
+    private static final String TAG = Exercise3Fragment.class.getSimpleName();
+
+    private static final int ONE_SECOND = 1000; // Milliseconds.
+    private static final int SECONDS_TO_COUNT = 3;
+
     private Button btnCountSeconds;
     private TextView textCount;
+    private Handler handler = new Handler(Looper.getMainLooper());
 
     public Exercise3Fragment() {
         // Required empty public constructor
@@ -47,6 +56,16 @@ public class Exercise3Fragment extends Fragment {
         return view;
     }
 
+    private void updateViews(final String countState, final Boolean enableButton) {
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                textCount.setText(countState);
+                btnCountSeconds.setEnabled(enableButton);
+            }
+        });
+    }
+
     private void countIterations() {
         /*
          * 1. Disable button to prevent multiple clicks.
@@ -54,5 +73,24 @@ public class Exercise3Fragment extends Fragment {
          * 3. Show count in TextView.
          * 4. When count completes, show "done" in TextView and enable the button.
          */
+
+        btnCountSeconds.setEnabled(false);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < SECONDS_TO_COUNT; i++) {
+                    try {
+                        updateViews(i + 1 + " s", false);
+                        Thread.sleep(ONE_SECOND);
+                    } catch (InterruptedException ex) {
+                        Log.e(TAG, ex.toString());
+                        return;
+                    }
+                }
+
+                updateViews("Done", true);
+            }
+        }).start();
     }
 }
