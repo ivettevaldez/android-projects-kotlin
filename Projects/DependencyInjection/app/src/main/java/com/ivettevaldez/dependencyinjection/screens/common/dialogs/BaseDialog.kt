a4b2013399dd9249b7d229f252ab37edd29f5c6e
Ivette Valdez <ivette.valdez.e@gmail.com>
2021-05-06 17:44:06 +0200

    DependencyInjection: Introduction of BaseDialog and DI infrastructure for dialogs.

--- Projects/DependencyInjection/app/src/main/java/com/ivettevaldez/dependencyinjection/screens/common/dialogs/BaseDialog.kt
+++ Projects/DependencyInjection/app/src/main/java/com/ivettevaldez/dependencyinjection/screens/common/dialogs/BaseDialog.kt
@@ -1,5 +1,14 @@
 package com.ivettevaldez.dependencyinjection.screens.common.dialogs
 
 import androidx.fragment.app.DialogFragment
+import com.ivettevaldez.dependencyinjection.common.dependencyinjection.presentation.PresentationComponent
+import com.ivettevaldez.dependencyinjection.screens.common.controllers.BaseActivity
 
-open class BaseDialog : DialogFragment()
+open class BaseDialog : DialogFragment() {
+
+    private val presentationComponent: PresentationComponent by lazy {
+        (requireActivity() as BaseActivity).activityComponent.newPresentationComponent()
+    }
+
+    protected val injector get() = presentationComponent
+}
