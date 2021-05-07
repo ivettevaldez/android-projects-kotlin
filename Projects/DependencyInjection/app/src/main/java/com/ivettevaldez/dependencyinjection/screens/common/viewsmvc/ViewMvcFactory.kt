40ee6c95c1360fa76291252ad6f6cc090ce161ef
Ivette Valdez <ivette.valdez.e@gmail.com>
2021-05-07 16:53:27 +0200

    DependencyInjection: Introduction to providers in ViewMvcFactory class.

--- Projects/DependencyInjection/app/src/main/java/com/ivettevaldez/dependencyinjection/screens/common/viewsmvc/ViewMvcFactory.kt
+++ Projects/DependencyInjection/app/src/main/java/com/ivettevaldez/dependencyinjection/screens/common/viewsmvc/ViewMvcFactory.kt
@@ -6,17 +6,18 @@ import com.ivettevaldez.dependencyinjection.screens.common.images.ImageLoader
 import com.ivettevaldez.dependencyinjection.screens.questiondetails.QuestionDetailsViewMvcImpl
 import com.ivettevaldez.dependencyinjection.screens.questionslist.QuestionsListViewMvcImpl
 import javax.inject.Inject
+import javax.inject.Provider
 
 class ViewMvcFactory @Inject constructor(
-    private val inflater: LayoutInflater,
-    private val imageLoader: ImageLoader
+    private val inflater: Provider<LayoutInflater>,
+    private val imageLoader: Provider<ImageLoader>
 ) {
 
     fun newQuestionsListViewMvc(parent: ViewGroup?): QuestionsListViewMvcImpl {
-        return QuestionsListViewMvcImpl(inflater, parent)
+        return QuestionsListViewMvcImpl(inflater.get(), parent)
     }
 
     fun newQuestionDetailsViewMvc(parent: ViewGroup?): QuestionDetailsViewMvcImpl {
-        return QuestionDetailsViewMvcImpl(inflater, parent, imageLoader)
+        return QuestionDetailsViewMvcImpl(inflater.get(), parent, imageLoader.get())
     }
 }
