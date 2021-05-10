ae7b3ff42ed80b328a7109640062cb2c4c4ba7e2
Ivette Valdez <ivette.valdez.e@gmail.com>
2021-05-10 18:08:50 +0200

    DependencyInjection: Fixing scopes.

--- Projects/DependencyInjection/app/src/main/java/com/ivettevaldez/dependencyinjection/common/dependencyinjection/activity/ActivityScope.kt
+++
@@ -1,6 +0,0 @@
-package com.ivettevaldez.dependencyinjection.common.dependencyinjection.activity
-
-import javax.inject.Scope
-
-@Scope
-annotation class ActivityScope
--- Projects/DependencyInjection/app/src/main/java/com/ivettevaldez/dependencyinjection/common/dependencyinjection/application/ApplicationScope.kt
+++ Projects/DependencyInjection/app/src/main/java/com/ivettevaldez/dependencyinjection/common/dependencyinjection/application/ApplicationScope.kt
@@ -1,6 +1,9 @@
 package com.ivettevaldez.dependencyinjection.common.dependencyinjection.application
 
+import dagger.hilt.migration.AliasOf
 import javax.inject.Scope
+import javax.inject.Singleton
 
 @Scope
+@AliasOf(Singleton::class)
 annotation class ApplicationScope
