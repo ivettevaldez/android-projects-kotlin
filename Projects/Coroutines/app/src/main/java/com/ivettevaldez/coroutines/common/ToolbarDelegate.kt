f9d5e327ee47eaee4ef3399295db536bc1d8062d
Ivette Valdez <ivette.valdez.e@gmail.com>
2023-01-25 12:58:33 +0100

    Remove old Coroutines project

--- Projects/Coroutines/app/src/main/java/com/ivettevaldez/coroutines/common/ToolbarDelegate.kt
+++
@@ -1,8 +0,0 @@
-package com.ivettevaldez.coroutines.common
-
-interface ToolbarDelegate {
-
-    fun setScreenTitle(title: String)
-    fun showUpButton()
-    fun hideUpButton()
-}
--- Projects/Coroutines/app/src/main/java/com/ivettevaldez/coroutines/common/dependencyinjection/ApplicationCompositionRoot.kt
+++
@@ -1,3 +0,0 @@
-package com.ivettevaldez.coroutines.common.dependencyinjection
-
-class ApplicationCompositionRoot
