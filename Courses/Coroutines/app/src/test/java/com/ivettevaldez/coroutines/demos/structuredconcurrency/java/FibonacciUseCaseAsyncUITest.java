8556ab7d5331f64a7bfa2d55ab79b18f21ebf285
Ivette Valdez <ivette.valdez.e@gmail.com>
2023-02-02 16:13:19 +0100

    Create Java FibonacciUseCaseAsyncUI with test

---
+++ Courses/Coroutines/app/src/test/java/com/ivettevaldez/coroutines/demos/structuredconcurrency/java/FibonacciUseCaseAsyncUITest.java
@@ -0,0 +1,62 @@
+package com.ivettevaldez.coroutines.demos.structuredconcurrency.java;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.math.BigInteger;
+
+public class FibonacciUseCaseAsyncUITest {
+
+    private FibonacciUseCaseAsyncUI SUT;
+    private FibonacciUseCaseAsyncUI.Callback callback;
+
+    private BigInteger lastResult = null;
+
+    @Before
+    public void setUp() {
+        SUT = new FibonacciUseCaseAsyncUI();
+        callback = result -> lastResult = result;
+    }
+
+    @Test
+    public void computeFibonacci_0_returns0() throws InterruptedException {
+        // Arrange
+        // Act
+        SUT.computeFibonacci(0, callback);
+        Thread.sleep(10);
+        // Assert
+        assertEquals(new BigInteger("0"), lastResult);
+    }
+
+    @Test
+    public void computeFibonacci_1_returns1() throws InterruptedException {
+        // Arrange
+        // Act
+        SUT.computeFibonacci(1, callback);
+        Thread.sleep(10);
+        // Assert
+        assertEquals(new BigInteger("1"), lastResult);
+    }
+
+    @Test
+    public void computeFibonacci_10_returnsCorrectValue() throws InterruptedException {
+        // Arrange
+        // Act
+        SUT.computeFibonacci(10, callback);
+        Thread.sleep(50);
+        // Assert
+        assertEquals(new BigInteger("55"), lastResult);
+    }
+
+    @Test
+    public void computeFibonacci_30_returnsCorrectValue() throws InterruptedException {
+        // Arrange
+        // Act
+        SUT.computeFibonacci(30, callback);
+        Thread.sleep(100);
+        // Assert
+        assertEquals(new BigInteger("832040"), lastResult);
+    }
+}
